7ae0695aa4af135074b34dfec24d329bfa1ba4e0
CommanderSpecialK <markus.k.2207@gmail.com>
2026-02-03 08:47:37 +0100

    app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if check_password():
     else:
         st.error("Bitte 'GEMINI_API_KEY' in den Secrets hinterlegen!")
 
-    model = genai.GenerativeModel('gemini-2.0-flash')
+    model = genai.GenerativeModel('gemini-1.5-flash')
 
     if 'alle_kontakte' not in st.session_state:
         st.session_state.alle_kontakte = []
